56c0365e78402f057ca99a187ee930a2c634aad3
Sagar <Sagar9980>
2021-03-27 23:49:40 +0545

    changes on js

--- script.js
+++ script.js
@@ -2,68 +2,61 @@ function getHistory() {
   return document.getElementById("his").innerText;
 }
 function printHistory(num) {
-  document.getElementById("his").innerText = num;
+  return (document.getElementById("his").innerText = num);
 }
 function getOutput() {
   return document.getElementById("ans").innerText;
 }
 function printOutput(num) {
-  if (num == "") {
-    document.getElementById("ans").innerText = num;
-  } else if (num == ".") {
-    document.getElementById("ans").innerText = num;
-  } else {
-    document.getElementById("ans").innerText = getFormattedNumber(num);
-  }
+  return (document.getElementById("ans").textContent = num);
 }
-function getFormattedNumber(num) {
-  let n = Number(num);
-  let value = n.toLocaleString("en");
-  return value;
-}
-function reverseNumberFormat(num) {
-  return Number(num.replace(/,/g, ""));
-}
-
 let number = document.getElementsByClassName("number");
 
 for (let i = 0; i < number.length; i++) {
   number[i].addEventListener("click", function () {
-    let output = reverseNumberFormat(getOutput());
+    let output = getOutput();
     if (output != NaN) {
       output = output + this.innerText;
-      console.log(output);
       printOutput(output);
     }
   });
 }
-
 let operator = document.getElementsByClassName("operator");
 for (let i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", function () {
     if (this.id == "clear") {
       printHistory("");
       printOutput("");
-    } else if (this.id == "backspace") {
-      var output = reverseNumberFormat(getOutput()).toString();
+    }
+    if (this.id == "backspace") {
+      let output = getOutput().toString();
       if (output) {
         output = output.substr(0, output.length - 1);
         printOutput(output);
+      }
+    }
+    if (this.id == "plus-minus") {
+      let output = getOutput().toString();
+      if (output) {
+        let minus = "-";
+        let result = minus.concat(output);
+        printOutput(result);
       }
     } else {
       let output = getOutput();
       let history = getHistory();
       if (output != "") {
-        output = reverseNumberFormat(output);
         history = history + output;
         if (this.id == "=") {
           let result = eval(history);
           printOutput(result);
           printHistory(history);
         } else {
-          history = history + this.id;
-          printHistory(history);
-          printOutput(0);
+          if (this.id != "backspace") {
+            history = history + this.id;
+            printHistory(history);
+            printOutput();
+          }
         }
       }
     }
